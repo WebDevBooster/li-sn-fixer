27f9c3e6a6979da59ba0a338276bb0c4346aba32
Alex Booster <webdevbooster@gmail.com>
2022-08-29 11:55:27 +0100

    Auto-expand about and experience sections.

--- myscript_A.js
+++ myscript_A.js
@@ -1,3 +1,25 @@
 $( document ).ready(function() {
 
+    let aboutBtn = document.querySelector("#about-section [id$=-clamped-content] > span:nth-child(2)");
+    let experienceBtns = document.querySelectorAll("#experience-section [id$=-clamped-content] > span:nth-child(2)");
+
+    const myInterval = setInterval(expandSections, 500);
+
+    function expandExperienceBtn(item) {
+        item.click();
+    }
+
+    function expandSections() {
+        if (aboutBtn && experienceBtns.length) {
+            clearInterval(myInterval);
+            aboutBtn.click();
+            experienceBtns.forEach(expandExperienceBtn);
+        } else {
+            setTimeout(function () {
+                aboutBtn = document.querySelector("#about-section [id$=-clamped-content] > span:nth-child(2)");
+                experienceBtns = document.querySelectorAll("#experience-section [id$=-clamped-content] > span:nth-child(2)");
+            }, 450);
+        }
+    }
+
 });
