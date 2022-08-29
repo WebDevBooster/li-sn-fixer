a63e065dad7a8b2ffccb1601e4db375fa2fb5613
Alex Booster <webdevbooster@gmail.com>
2022-08-29 20:04:26 +0100

    Add audio highlighting.

--- myscript_A.js
+++ myscript_A.js
@@ -4,14 +4,54 @@ $( document ).ready(function() {
     let experienceBtns = document.querySelectorAll("#experience-section [id$=-clamped-content] > span:nth-child(2)");
 
     const aboutOpener = setInterval(expandAboutSection, 500);
-    const ExperienceOpener = setInterval(expandExperienceSections, 500);
+    const ExperienceOpener = setInterval(expandExperienceSections, 700);
 
     function expandExperienceBtn(item) {
         item.click();
     }
 
     function highlight() {
-        document.querySelector("#content-main").innerHTML = document.querySelector("#content-main").innerHTML.replace(/crypto/ig,"<mark>CRYPTO</mark>");
+        let currentHTML = document.querySelector("#content-main").innerHTML;
+        let keyword1Array = currentHTML.match(/crypto/gi);
+        let keyword2Array = currentHTML.match(/enthusiast/gi);
+        let emailsArray = currentHTML.match(/@/g);
+
+        if (keyword1Array && keyword2Array && emailsArray) {
+            $( "body" ).append('<audio id="LNSNF-kw1and2" autoplay><source src="https://alexbooster.com/media/adara.mp3"></audio>');
+            $( "body" ).append('<audio id="LNSNF-email" autoplay><source src="https://alexbooster.com/media/Cha-Ching.ogg"></audio>');
+            // User interaction/click required to play audio after page load:
+            // https://developer.chrome.com/blog/autoplay/
+            $( "#profile-card-section section[class^=_header_] h1" ).click(function() {
+                const playKW1and2Promise = document.querySelector('#LNSNF-kw1and2').play();
+                setTimeout(function () {
+                    const playEmailPromise = document.querySelector('#LNSNF-email').play();
+                }, 500);
+            });
+        } else if (keyword1Array && keyword2Array) {
+            $( "body" ).append('<audio id="LNSNF-kw1and2" autoplay><source src="https://alexbooster.com/media/adara.mp3"></audio>');
+            // User interaction/click required to play audio after page load:
+            // https://developer.chrome.com/blog/autoplay/
+            console.log($( "#profile-card-section section[class^=_header_] h1" ));
+            $( "#profile-card-section section[class^=_header_] h1" ).click(function() {
+                const playKW1and2Promise = document.querySelector('#LNSNF-kw1and2').play();
+            });
+        } else if (!keyword1Array && keyword2Array && emailsArray) {
+            $( "body" ).append('<audio id="LNSNF-kw2" autoplay><source src="https://alexbooster.com/media/Tones.ogg"></audio>');
+            $( "body" ).append('<audio id="LNSNF-email" autoplay><source src="https://alexbooster.com/media/Cha-Ching.ogg"></audio>');
+            $( "#profile-card-section section[class^=_header_] h1" ).click(function() {
+                const playKW2Promise = document.querySelector('#LNSNF-kw2').play();
+                setTimeout(function () {
+                    const playEmailPromise = document.querySelector('#LNSNF-email').play();
+                }, 500);
+            });
+        } else if (!keyword1Array && keyword2Array) {
+            $( "body" ).append('<audio id="LNSNF-kw2" autoplay><source src="https://alexbooster.com/media/Tones.ogg"></audio>');
+            $( "#profile-card-section section[class^=_header_] h1" ).click(function() {
+                const playKW2Promise = document.querySelector('#LNSNF-kw2').play();
+            });
+        }
+
+        // document.querySelector("#content-main").innerHTML = currentHTML.replace(/crypto/gi,`<mark style="font-weight: normal;">Crypto</mark>`);
     }
 
     function expandAboutSection() {
@@ -35,7 +75,7 @@ $( document ).ready(function() {
         } else {
             setTimeout(function () {
                 experienceBtns = document.querySelectorAll("#experience-section [id$=-clamped-content] > span:nth-child(2)");
-            }, 450);
+            }, 550);
         }
     }
 
