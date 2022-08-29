bc7e2990b4bd4480a3fb29d29210a26dbcd61b6b
Alex Booster <webdevbooster@gmail.com>
2022-08-29 15:44:43 +0100

    Update auto-expand.

--- myscript_A.js
+++ myscript_A.js
@@ -3,23 +3,37 @@ $( document ).ready(function() {
     let aboutBtn = document.querySelector("#about-section [id$=-clamped-content] > span:nth-child(2)");
     let experienceBtns = document.querySelectorAll("#experience-section [id$=-clamped-content] > span:nth-child(2)");
 
-    const myInterval = setInterval(expandSections, 500);
+    const aboutOpener = setInterval(expandAboutSection, 500);
+    const ExperienceOpener = setInterval(expandExperienceSections, 500);
 
     function expandExperienceBtn(item) {
         item.click();
     }
 
-    function expandSections() {
-        if (aboutBtn && experienceBtns.length) {
-            clearInterval(myInterval);
+    function expandAboutSection() {
+        if (aboutBtn) {
+            clearInterval(aboutOpener);
+            console.log("aboutBtn.click()...");
             aboutBtn.click();
-            experienceBtns.forEach(expandExperienceBtn);
+            setTimeout(highlight, 500);
         } else {
             setTimeout(function () {
                 aboutBtn = document.querySelector("#about-section [id$=-clamped-content] > span:nth-child(2)");
+            }, 450);
+        }
+    }
+
+    function expandExperienceSections() {
+        if (experienceBtns.length) {
+            clearInterval(ExperienceOpener);
+            console.log("triggering experienceBtns.forEach...");
+            experienceBtns.forEach(expandExperienceBtn);
+        } else {
+            setTimeout(function () {
                 experienceBtns = document.querySelectorAll("#experience-section [id$=-clamped-content] > span:nth-child(2)");
             }, 450);
         }
     }
+
 
 });
