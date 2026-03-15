4c0ef487eaa31700dd4b9a20caf5e139aa7a53e3
Alex Booster <webdevbooster@gmail.com>
2026-03-15 11:05:51 +0000

    Back up version 2.1.4

---
+++ v2.1.4/README.md
@@ -0,0 +1,2 @@
+# li-sn-fixer
+Browser add-on to fix various stupidities and annoyances in Linkedin Sales Navigator.
---
+++ v2.1.4/snf-search-page.js
@@ -0,0 +1,90 @@
+if (linkedinProfilePageMatch) {
+    console.log("Linkedin profile page detected");
+    waitFor("#artdeco-modal-outlet .artdeco-modal.send-invite textarea#custom-message").then((el) => {
+        console.log("customMessageBox detected!");
+        const customMessageBox = document.querySelector("#artdeco-modal-outlet .artdeco-modal.send-invite textarea#custom-message");
+        setInterval(function () {
+            if (customMessageBox) {
+                customMessageBox.style.height = "180px";
+            }
+        }, 250);
+    });
+}
+
+if (searchPageMatch) {
+    waitFor("#search-results-container ol li:nth-of-type(1)").then((el) => {
+        updateLocalStorage();
+
+        function manipulateListElement(element, index) {
+            if (element.querySelector("span[data-anonymize=title]") !== null) {
+                const spanElement = element.querySelector("span[data-anonymize=title]");
+                const spanText = spanElement.textContent;
+
+                if (/project/i.test(spanText)) {
+                    const highlightedText = spanText.replace(/project/gi, '<span style="background-color: peachpuff;">Project</span>');
+                    spanElement.innerHTML = highlightedText;
+                }
+            }
+
+            // Hide non-premium profiles (for now I'm only targeting premium profiles)
+            if (element.querySelector("span[data-anonymize=person-name]") !== null
+                && element.querySelector("li-icon[type=linkedin-premium-gold-icon]") === null
+                && element.querySelector("li-icon[aria-label^=Viewed]") !== null
+                && element.querySelector(".SNF-viewed-non-premium") === null) {
+                element.classList.add("SNF-viewed-non-premium");
+            }
+
+            if (element.querySelector("span[data-anonymize=person-name]") !== null
+                && element.querySelector("li-icon[type=linkedin-premium-gold-icon]") === null
+                && element.querySelector(".SNF-non-premium") === null) {
+                element.classList.add("SNF-non-premium");
+            }
+
+            if (element.querySelector("li-icon[aria-label^=Viewed]") !== null && element.querySelector(".SNF-viewed-premium") === null) {
+                element.classList.add("SNF-viewed-premium");
+            }
+
+            // most "out-of-network" profiles are useless for me
+            if (element.querySelector("div[class^=_out-of-network]") !== null && element.querySelector(".SNF-out-of-network") === null) {
+                element.classList.add("SNF-out-of-network");
+            }
+
+            // Highlight list items that are already in my database
+            urlsPromise.then(() => {
+                if (idsSet) {
+                    highlightMatchingListItems(idsSet);
+                } else {
+                    console.warn("idsSet is undefined - JSON might not have loaded.");
+                }
+            });
+        }
+
+        function handleListItems() {
+            const listArray = document.querySelectorAll("li.artdeco-list__item");
+            const aboutSectionsArray = document.querySelectorAll("li.artdeco-list__item > div > div > div:nth-of-type(2) > div > div:nth-of-type(2)");
+            const yearsArray = document.querySelectorAll("li.artdeco-list__item > div > div > div:nth-of-type(2) > div > div > div > div:nth-of-type(2) > div:nth-of-type(4)");
+
+            if (listArray.length) {
+                listArray.forEach(manipulateListElement);
+            }
+
+            if (aboutSectionsArray.length) {
+                aboutSectionsArray.forEach(function (el) {
+                    if (el.offsetParent !== null) {
+                        el.style.display = "none";
+                    }
+                });
+            }
+
+            if (yearsArray.length) {
+                yearsArray.forEach(function (el) {
+                    if (el.offsetParent !== null) {
+                        el.style.display = "none";
+                    }
+                });
+            }
+        }
+
+        setInterval(handleListItems, 300);
+    });
+}
